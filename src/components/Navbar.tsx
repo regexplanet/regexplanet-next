7365fb236a0aaa9071986ef8076f6fce18ef5481
Andrew Marcuse <fileformat@gmail.com>
2024-10-26 14:18:53 -0400

    Add link to Regex.zone for patterns

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,14 @@
 
 import Link from "next/link";
 import { NavbarLink } from "./NavbarLink";
-import { PiCloudCheck, PiCloudCheckBold, PiPlay, PiPlayBold, PiQuestion, PiQuestionBold } from "react-icons/pi";
+import { PiBlueprint, PiBlueprintBold, PiCloudCheck, PiCloudCheckBold, PiPlay, PiPlayBold, PiQuestion, PiQuestionBold } from "react-icons/pi";
 
 /* eslint-disable @next/next/no-img-element */
 
 
 const links = [
     { link: '/', label: 'Testing', startsWith: '/advanced/', icon: <PiPlay />, icon_bold: <PiPlayBold /> },
+    { link: 'https://www.regex.zone/patterns/', label: 'Patterns', startsWith: '/patterns/', icon: <PiBlueprint />, icon_bold: <PiBlueprintBold /> }, 
     {
         link: '/status.html', label: 'Status', startsWith: '/status.html', icon: <PiCloudCheck />, icon_bold: <PiCloudCheckBold />
     },
